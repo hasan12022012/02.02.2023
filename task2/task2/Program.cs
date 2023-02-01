0081867deb1240653f6a69f856ef2781dccaae0b
Hasan Huseynli <hasansh@code.edu.az>
2023-02-02 00:56:27 +0400

    taskDownloaded

---
+++ task2/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş yazıda rəqəm olub olmadığını tapan metod.Göndərilən yazıda rəqəm varsa true yoxdursa false qaytarır.
+            Console.WriteLine("Hello everyone how are you");
+            Console.Write("Insert word");
+            string text = Console.ReadLine();
+            Console.WriteLine(ContainsNumber(text));
+
+        }
+        static bool ContainsNumber(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ task3/task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş yazıdaki sözlərin sayını tapan metod.Boşluqla bir birindən ayrılmış bütün ifadələri söz kimi qəbul edin.
+            //Və hər söz arasında bir boşluq varmış kimi hesab edin.
+
+            Console.WriteLine("Verilmiş yazıdaki sözlerin sayanı tapan metod.");
+            Console.WriteLine("Text'i daxil edin");
+            string text = Console.ReadLine();
+            Console.WriteLine(count2(text));
+
+            ;       }
+        static int CountWords1(string text)
+        {
+            int count1 = 0;
+            bool wordStarted = false;
+            foreach (char c in text)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    wordStarted = false;
+                }
+                else if (!wordStarted)
+                {
+                    wordStarted = true;
+                    count1++;
+                }
+            }
+            return count1;
+        }
+        static int count2(string text)
+        {
+            string[] words = text.Split(' ');
+            int count2 = words.Length;
+
+            return count2;
+        }
+    }
+}
+
+
+    
+
