9efac11e2240a7d0c082457ec451f70aac018b60
Hasan Huseynli <hasansh@code.edu.az>
2023-02-02 00:57:41 +0400

    yoxlamalar

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmis ededler siyahisina verilmis ededi elave edib arrayi qaytaran metod
+            int[] array = { 10, 33, 56 };
+            Add(array, 12);
+
+        }
+
+        static int[] Add(int[] arr, int num)
+        {
+            int[] newArr =new int[arr.Length + 1];
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                newArr[i] = arr[i];
+
+            }
+            newArr[newArr.Length-1]=num;
+
+            return newArr;
+        }
+        
+        
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(char.IsDigit('4'));
+            Console.WriteLine(char.IsLetter('5'));
+            Console.WriteLine(char.IsLetterOrDigit('A'));
+
+            var letter=char.ToUpper('a');
+            Console.WriteLine(letter);
+
+            letter = char.ToLower('A');
+            Console.WriteLine(letter);
+
+            Console.WriteLine(char.IsUpper('a'));
+            Console.WriteLine(char.IsLower('a'));
+
+
+            string text3 = "";
+            string newtext3 = text3.Substring(6, 11);
+            Console.Write(newtext3);
+            Console.Read();
+
+
+
+
+
+            string text1 = "     Hi everyone     ";
+            string newtext1 = text1.Trim();
+            Console.WriteLine(newtext1);
+
+
+
+
+            string text2 = "    Hi everyone     ";
+            int newtext2 = text2.Length;
+
+            Console.WriteLine(newtext2);
+
+           
+
+            
+        }
+    }
+}
